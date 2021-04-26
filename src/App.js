34e1145e097e8513f2dd304188bcc283440c0e4b
Garrick Suemith <garrick.suemith@gmail.com>
2021-04-26 13:08:34 -0400

    created login, fixed path issue

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Home from "./routes/home/Home";
+import Login from "./routes/login/Login";
 
 function App() {
   return (
     <Switch>
-      {/**<PrivateRoute to="/account/:id" /> */}
-      {/**<PrivateRoute to="/recipes/add" /> */}
-      {/**<PrivateRoute to="/recipes/:id/edit" /> */}
-      {/**<Route to="/recipes/:id" /> */}
-      {/**<Route to="/recipes" /> */}
-      {/**<Route to="/login" /> */}
-      {/**<Route to="/register" /> */}
-      <Route to="/" component={Home}/>
+      {/**<PrivateRoute path="/account/:id" /> */}
+      {/**<PrivateRoute path="/recipes/add" /> */}
+      {/**<PrivateRoute path="/recipes/:id/edit" /> */}
+      {/**<PrivateRoute path="/recipes/:id" /> */}
+      {/**<Route path="/recipes" /> */}
+      {/**<Route path="/register" /> */}
+
+      <Route path="/login" component={Login}/>
+      <Route path="/" component={Home}/> 
     </Switch>
   );
 }
